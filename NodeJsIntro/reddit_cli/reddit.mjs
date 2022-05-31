074c4ae72521b7719eecde780960654fc0bd57d9
sibteali786 <sibteali786@gmail.com>
2022-05-31 17:26:31 +0500

    made a working CLI for Reddit random posts

--- NodeJsIntro/reddit_cli/reddit.mjs
+++ NodeJsIntro/reddit_cli/reddit.mjs
@@ -1,3 +1,23 @@
 #! /usr/bin/env node
- 
-console.log("Hello Reddit")
+import fetch from "node-fetch";
+import open from "open";
+import yargs from 'yargs';
+
+// Dealing the with flags passsed to the reddit command when we write it in browser
+// console.log(process.argv)
+const {argv} = yargs(process.argv)
+const res = await fetch("https://reddit.com/.json")
+const data = await res.json()
+const children = data.data.children 
+const randomPost = children[Math.floor(Math.random() * data.data.children.length)]
+const link = `https://reddit.com${randomPost.data.permalink}` 
+// console.log(data);
+
+if (argv.print){
+    console.log(`
+        title : ${randomPost.data.title},
+        link:${link}
+    `);
+}else {
+    open(link)
+}
